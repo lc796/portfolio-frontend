05888103051b7b2517abb0c4cc46f04dd1ac781b
Luke Collishaw <lc796@exeter.ac.uk>
2022-10-01 12:21:07 +0100

    Update footer to include phone number and email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,13 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 const Footer = () => {
     return (
         <div className="Footer">
-            <div className="wrapper">
-                <a href="https://github.com/lc796"><FontAwesomeIcon className="footer-social-icon" icon={faGithub}/></a>
-                <a href="linkedin.com/in/luke-collishaw-2240a4159"><FontAwesomeIcon className="footer-social-icon" icon={faLinkedin}/></a>
-                <a href="mailto:lukecollishaw@gmail.com"><FontAwesomeIcon className="footer-social-icon" icon={faEnvelope}/></a> lukecollishaw@gmail.com
-                <FontAwesomeIcon className="footer-social-icon" icon={faPhoneSquare}/> 07539696081
+            <div className="footer-container">
+                <div id="footer-wrapper">
+                    <a href="https://github.com/lc796"><FontAwesomeIcon className="footer-social-icon" icon={faGithub}/></a>
+                    <a href="linkedin.com/in/luke-collishaw-2240a4159"><FontAwesomeIcon className="footer-social-icon" icon={faLinkedin}/></a>
+                </div>
+                <div><a href="mailto:lukecollishaw@gmail.com"><FontAwesomeIcon className="footer-social-icon" icon={faEnvelope}/></a> lukecollishaw@gmail.com</div>
+                <div><FontAwesomeIcon className="footer-social-icon" icon={faPhoneSquare}/> 07539696081</div>
             </div>
         </div>
     );
