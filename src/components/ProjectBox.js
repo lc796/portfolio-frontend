160c0356d5f19639f5956abd7a41045bda3f431a
Luke Collishaw <lc796@exeter.ac.uk>
2022-10-04 18:42:32 +0100

    Add project tag to end of main project box body

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -1,11 +1,11 @@
 import {Fragment, useState} from "react";
 import {faGithub, faJava, faJs, faReact, faPhp, faPython, faCss3, faHtml5, faGit} from '@fortawesome/free-brands-svg-icons'
-import {faGlobe} from "@fortawesome/free-solid-svg-icons";
+import {faDatabase, faGlobe} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import TechLanguageTooltip from "./TechLanguageTooltip";
 
 const ProjectBox = (props) => {
-    const {id, name, description, languages, tooltip, links} = props.project;
+    const {id, name, description, languages, tooltip, links, tag} = props.project;
 
     const [isHovering, setIsHovering] = useState(false);
 
@@ -21,39 +21,23 @@ const ProjectBox = (props) => {
     const techUsed = languages.map((language) => {
         switch (language) {
             case "React":
-                //return <li key={language}><img alt="React logo" src={react}/></li>;
                 return <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} key={language}><FontAwesomeIcon icon={faReact}/></li>;
             case "HTML":
-                //return <li key={language}><img alt="HTML logo" src={html}/></li>;
                 return <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} key={language}><FontAwesomeIcon icon={faHtml5}/></li>;
             case "CSS":
-                //return <li key={language}><img alt="CSS logo" src={css}/></li>;
                 return <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} key={language}><FontAwesomeIcon icon={faCss3}/></li>;
-            //case "Springboot":
-                //return <li key={language}><img alt="Spring Boot logo" src={springboot}/></li>;
-            //case "Apache":
-                //return <li key={language}><img alt="Apache logo" src={apache}/></li>;
-            //case "MySQL":
-                //return <li key={language}><img alt="MySQL logo" src={mysql}/></li>;
+            case "MySQL":
+                return <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} key={language}><FontAwesomeIcon icon={faDatabase}/></li>;
             case "JS":
-                //return <li key={language}><img alt="JavaScript logo" src={js}/></li>;
                 return <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} key={language}><FontAwesomeIcon icon={faJs}/></li>;
             case "Git":
-                //return <li key={language}><img alt="Git logo" src={git}/></li>;
                 return <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} key={language}><FontAwesomeIcon icon={faGit}/></li>;
             case "Java":
-                //return <li key={language}><img alt="Java logo" src={java}/></li>
                 return <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} key={language}><FontAwesomeIcon icon={faJava}/></li>;
-            //case "Minecraft":
-                //return <li key={language}><img alt="Minecraft logo" src={minecraft}/></li>
             case "Python":
-                //return <li key={language}><img alt="Python logo" src={python}/></li>
                 return <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} key={language}><FontAwesomeIcon icon={faPython}/></li>;
             case "PHP":
-                //return <li key={language}><img alt="PHP logo" src={php}/></li>
                 return <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} key={language}><FontAwesomeIcon icon={faPhp}/></li>;
-            //case "Flask":
-                //return <li key={language}><img alt="Flask logo" src={flask}/></li>
             default:
                 return <li key={language}></li>
         }
@@ -81,6 +65,7 @@ const ProjectBox = (props) => {
                     <TechLanguageTooltip tooltip={tooltip}/>
                 }
                 <p>{description}</p>
+                <div className="tag">{tag}</div>
             </div>
             <div className="project-footer">
                 {renderContacts()}
