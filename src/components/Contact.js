a70728254c8eaaee0004c5d562182266e5d2b2e8
Luke Collishaw <lc796@exeter.ac.uk>
2022-10-09 16:39:05 +0100

    Update form to use Netlify

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,15 +4,15 @@ const Contact = () => {
             <div className="wrapper">
                 <h1>Contact</h1>
                 <hr/>
-                <form action="">
+                <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
                     <div className="form-container">
                         <div className="form-section">
                             <label htmlFor="name">Name:</label><br/>
-                            <input type="text" id="name" name="name" placeholder="Test"/>
+                            <input type="text" id="name" name="name"/>
                         </div>
                         <div className="form-section">
-                            <label htmlFor="age">Age:</label><br/>
-                            <input type="text" id="age" name="age" placeholder="Test"/>
+                            <label htmlFor="age">Email:</label><br/>
+                            <input type="text" id="age" name="age"/>
                         </div>
                         <div className="form-section">
                             <button>Get in touch!</button>
