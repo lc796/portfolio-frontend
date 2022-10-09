dc9cf9e91c542341efcaff0a2c4917d96997eefa
Luke Collishaw <lc796@exeter.ac.uk>
2022-10-09 17:23:03 +0100

    Try to get working with Netlify...

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,11 +4,11 @@ const Contact = () => {
             <div className="wrapper">
                 <h1>Contact</h1>
                 <hr/>
-                <form name="contact" method="POST" data-netlify="true">
+                <form name="contact-form" method="POST" data-netlify="true">
                     <label htmlFor="name">Name:</label>
                     <input type="text" id="name" name="name"/>
-                    <label htmlFor="age">Email:</label>
-                    <input type="text" id="age" name="age"/>
+                    <label htmlFor="email">Email:</label>
+                    <input type="text" id="email" name="email"/>
                     <button>Get in touch!</button>
                 </form>
             </div>
