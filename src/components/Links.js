855c94e1e2b89b6c719c1d7858616a8ab5895e07
Luke Collishaw <lc796@exeter.ac.uk>
2022-10-10 20:18:00 +0100

    Add CV to navbar, closes #12

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,10 +1,12 @@
 import {Fragment} from "react";
+import cv from "../static/CV.pdf";
 
 const Links = () => {
     const links = [
         {name: "about", url: "/#about"},
         {name: "projects", url: "/#projects"},
-        {name: "contact", url: "/#contact"}
+        {name: "contact", url: "/#contact"},
+        {name: "view cv", url: cv}
     ]
 
     return (
@@ -12,6 +14,7 @@ const Links = () => {
             <li><a href={links[0].url}>{links[0].name}</a></li>
             <li><a href={links[1].url}>{links[1].name}</a></li>
             <li><a href={links[2].url}>{links[2].name}</a></li>
+            <li><a className="cv" target="_blank" href={links[3].url}>{links[3].name}</a></li>
         </Fragment>
     )
 }
