84e4cc7a18fc30b9eda75d13326832d305eb4656
Luke Collishaw <lc796@exeter.ac.uk>
2022-10-10 20:25:10 +0100

    Change splash header for small screens, closes #13

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -6,7 +6,7 @@ const Splash = () => {
     return (
         <div className="splash-container">
             <div className="splash">
-                <h1>Hi, I'm Luke</h1>
+                <h1>Hi</h1>
                 <p>I am a software engineer from the United Kingdom. My main focus is front-end web development, and I enjoy developing React applications.</p>
                 <button className="cta" onClick={handleCallToAction}>Work with me!</button>
             </div>
