d3436e33689e927dcfd130fec61a21e69619465c
Luke Collishaw <lc796@exeter.ac.uk>
2022-10-11 11:50:45 +0100

    Make splash CTA link to projects, closes #10

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,14 +1,10 @@
-const handleCallToAction = () => {
-    console.log("cta")
-}
-
 const Splash = () => {
     return (
         <div className="splash-container">
             <div className="splash">
                 <h1>Hi</h1>
                 <p>I am a software engineer from the United Kingdom. My main focus is front-end web development, and I enjoy developing React applications.</p>
-                <button className="cta" onClick={handleCallToAction}>Work with me!</button>
+                <a className="cta" href="#projects">View projects</a>
             </div>
         </div>
     );
