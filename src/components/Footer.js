43e0ffbb2430a823027cc1485f4e7edc2e8dce32
Luke Collishaw <lc796@exeter.ac.uk>
2022-10-25 13:53:34 +0100

    Fix LinkedIn link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
             <div className="footer-container">
                 <div id="footer-wrapper">
                     <a href="https://github.com/lc796"><FontAwesomeIcon className="footer-social-icon" icon={faGithub}/></a>
-                    <a href="linkedin.com/in/luke-collishaw-2240a4159"><FontAwesomeIcon className="footer-social-icon" icon={faLinkedin}/></a>
+                    <a href="https://linkedin.com/in/luke-collishaw-2240a4159"><FontAwesomeIcon className="footer-social-icon" icon={faLinkedin}/></a>
                 </div>
                 <div><a href="mailto:lukecollishaw@gmail.com"><FontAwesomeIcon className="footer-social-icon" icon={faEnvelope}/></a> lukecollishaw@gmail.com</div>
                 <div><FontAwesomeIcon className="footer-social-icon" icon={faPhoneSquare}/> 07539696081</div>
