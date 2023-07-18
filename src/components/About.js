44a7d3e4033155156cd92d20ac239b49a3f813c8
Luke Collishaw <luke.collishaw@hatless.studio>
2023-07-18 02:12:16 +0100

    Update splash copy

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,27 @@ const About = () => {
                 <hr/>
                 <div className="about-container">
                     <div className="about-item">
-                        <h2>What I can do</h2>
-                        <p>I can design beautiful front-end websites using HTML/CSS/JS/React, alongside various other frameworks and libraries. I also have experience with back-end development working with a variety of languages, including SQL, Java, Python, and PHP. Technologies I have used include: Linux, Git, NodeJS, Apache, MySQL, PostgreSQL, Django, Flask, Spring Boot, Bukkit/Spigot API, Xenforo, and MyBB. If you have something else that I might be able to help with, why not get in contact? <b>All quotes free of charge.</b></p>
+                        <div className="about-item-text">
+                            <h2>What I can do</h2>
+                            <p>I can develop beautiful front-end websites using HTML/CSS/JS/React, alongside various other frameworks and libraries</p>
+                            <p>I have experience with back-end development working with a variety of languages and technologies, including modern HTML and CSS, JavaScript (alongside React and Next), and Python (with Django)</p>
+                            <p>I aim able to work and communicate in a team effectively using Agile/Kanban and Git 💬</p>
+                        </div>
                     </div>
                     <div className="about-item">
                         <h2>My experience</h2>
-                        <p>I am expecting to graduate with a <b>First Class Honours in BSc Computer Science</b> from the <b>University of Exeter</b>. This has given me the chance to learn numerous programming languages, and gain experience both collaborating with teams and working on my own. I pride myself on going beyond the code. This includes producing high quality tests, documentation, and support with deployment and maintenance.</p>
+                        <div className="about-item-text">
+                            <p>I am expecting to graduate with a <b>First Class Honours in BSc Computer Science</b> from the <b>University of Exeter</b> 🎓</p>
+                            <p>This has given me the chance to learn numerous programming languages, and gain experience both collaborating with teams and working on my own. I pride myself on going beyond the code. This includes producing high quality tests, documentation, and support with deployment and maintenance</p>
+
+                        </div>
                     </div>
                     <div className="about-item">
                         <h2>More about me</h2>
-                        <p>I am someone who is keen to learn and improve, and I always approach tasks with humility. I enjoy working out and training, and have competed in amateur boxing bouts. I also enjoy hiking, nature and animals. And, of course, no programmer is complete without having acquired a taste for coffee.</p>
+                        <div className="about-item-text">
+                            <p>I am someone who is keen to learn and improve, and I always approach any aspect of my life with confidence and an openminded willing to learn</p>
+                            <p>I also enjoy working out and training, and have competed in amateur boxing bouts, alongside a love of hiking, nature, and the outdoors. And as a software developer, it goes without saying a <s>problem</s>love for coffee ☕</p>
+                        </div>
                     </div>
                 </div>
             </div>
